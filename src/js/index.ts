3f661a2bf1335d8d59622b28f06a99d6ba421986
zentarg <apg.chili@gmail.com>
2020-10-27 14:01:50 +0100

    Task 1 & 2. Including showing error message in html

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,36 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+import { ICar } from "ICar";
+
+let carAPI = "https://webapicar20190326034339.azurewebsites.net/api/cars";
+
+let carDataContainer = document.getElementById("carData");
+let carDataError = document.getElementById("carDataError");
+
+document.getElementById("getCarDataButton").addEventListener("click", GetAllCars);
+
+function CreateCarListElement(car : ICar) {
+    let li : HTMLLIElement = document.createElement("li");
+    let liText : Text = document.createTextNode(`${car.id} | Vendor: ${car.vendor} | Model: ${car.model} | Price: ${car.price}`);
+    li.appendChild(liText);
+    return li;
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+function GetAllCars() {
+    carDataError.innerText = "";
+    axios.get<ICar[]>(carAPI).then((response : AxiosResponse<ICar[]>) => {
+        console.log(response);
+        carDataContainer.innerHTML = "";
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+        response.data.forEach((car : ICar) => {
+            let newLI : HTMLLIElement = CreateCarListElement(car);
+            carDataContainer.appendChild(newLI);
+        });
+
+    }).catch((error : AxiosError) => {
+        console.log(error);
+        carDataError.innerText = error.toJSON.toString();
+    });
+}
